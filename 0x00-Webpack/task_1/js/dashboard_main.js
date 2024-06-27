f558196ef3dd96933d8a2c18f9cbf56c3ef84c1e
Ayoub-oumha <Ayyouboumha@gmail.com>
2024-06-27 19:35:31 +0000

    commit

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import _ from 'lodash';
 
 $(document).ready(function() {
   $('body').append('<p>Holberton Dashboard</p>');
@@ -6,4 +7,14 @@ $(document).ready(function() {
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('button').on('click', _.debounce(updateCounter, 500));
 });
+
